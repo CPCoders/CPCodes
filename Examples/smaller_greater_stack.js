00c8a8d24900ca259c11c0b6dc921b4402000fc4
Prithwiraj Samanta <prskid1000@gmail.com>
2020-10-19 08:29:00 +0530

    examples added

---
+++ Examples/smaller_greater_stack.js
@@ -0,0 +1,6 @@
+var ele = require("../lib/smaller_greater_stack.js");
+var arr = [4,5,3,6,1,0,-1];
+console.log(ele.next_greater_left(arr));
+console.log(ele.next_smaller_left(arr));
+console.log(ele.next_greater_right(arr));
+console.log(ele.next_smaller_right(arr));
